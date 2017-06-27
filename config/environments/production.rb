64fc4b3dc462130f3942fa752c07a52b73bc276a
nik <worldnillin@gmail.com>
2017-06-28 01:38:15 +0300

    change smtp server second

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -70,7 +70,7 @@ Rails.application.configure do
       password: Rails.application.secrets.email_provider_password
   }
   # ActionMailer Config
-  config.action_mailer.default_url_options = { :host => 'gmail.com'}
+  config.action_mailer.default_url_options = { :host => 'maxim-rada.ru'}
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = false
