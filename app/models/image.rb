d53a0cbd213e7924f6081ca91fa997f470f0bc9d
nik <worldnillin@gmail.com>
2017-07-01 03:27:57 +0300

    change css

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,7 +1,7 @@
 class Image < ApplicationRecord
   resourcify
 
-  has_attached_file :avatar, :styles => { :medium => "270x270#", :thumb => "100x100>" }, :default_url => "/images/:style/missing_thumb34.png"
+  has_attached_file :avatar, :styles => { :medium => "300x300#", :thumb => "100x100>" }, :default_url => "/images/:style/missing_thumb34.png"
   validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
 
   belongs_to :album
