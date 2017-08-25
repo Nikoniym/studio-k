46c29b874a92fc6be8a1648064ba30e9510512ba
nik <worldnillin@gmail.com>
2017-08-25 22:15:25 +0300

    images 12 16 20

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -1,7 +1,10 @@
 class AlbumsController < ApplicationController
   def load_gallery
     @album = Album.find(params[:id])
-    @images = @album.images.order("RANDOM()").limit(12)
+    limit = @album.images.size
+    limit = (1..limit).to_a.find_all{ |elem| elem % 4 == 0 }
+    limit = limit[-1] if limit.present?
+    @images = @album.images.order("RANDOM()").limit(limit.blank? || limit<=12 ? 12 : limit)
 
     respond_to do |format|
       format.js
