a937f35a79a86288ad4dc440db2018ff15c5ab30
nik <worldnillin@gmail.com>
2017-08-26 02:11:16 +0300

    backup

--- backup/models/rails_database.rb
+++ backup/models/rails_database.rb
@@ -10,8 +10,9 @@
 # http://meskyanichi.github.io/backup
 #
 Model.new(:rails_database, 'Backups of the Rails Database') do
-  archive :mayak_archive do |archive|
-    archive.add "/backup/"
+  archive :app_archive do |archive|
+    archive.use_sudo
+    archive.add '/home/nik/deploy/'
   end
 
   database PostgreSQL do |db|
