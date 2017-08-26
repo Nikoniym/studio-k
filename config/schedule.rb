6331f072cb3c43749364d2cddacfe2881a0cdfb2
nik <worldnillin@gmail.com>
2017-08-26 12:31:02 +0300

    cron

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,11 +1,11 @@
 require 'dotenv/load'
 
 set :output, "#{path}/log/cron.log"
-set :my_password, "#{ENV['MY_PASSWORD']}"
+
 
 job_type :backup, "cd :path/:backup_path && :environment_variable=:environment bundle exec backup perform -t :task --config_file ./config.rb :output"
 
-job_type :renew, "/home/nik/letsencrypt :task :function"
+job_type :renew, "/home/nik/letsencrypt echo '#{ENV['MY_PASSWORD']}' | sudo -S :task :function"
 
 every :month do
   renew './letsencrypt-auto', function: 'renew'
