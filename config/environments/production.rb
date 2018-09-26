9656ae7cccdd17609b9d90ae68d5b23f05051671
nik <worldnillin@gmail.com>
2018-09-26 03:15:32 +0300

    environments production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -4,13 +4,11 @@ Rails.application.configure do
   # Code is not reloaded between requests.
   config.cache_classes = true
   config.force_ssl = false
-  config.assets.js_compressor = :uglifier
-  # config.assets.css_compressor = :sass
+  config.assets.js_compressor :yui
+  config.assets.css_compressor :yui
+  config.assets.compile = true
 
-  # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
-
-                               # Eager load code on boot. This eager loads most of Rails and
+  # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
   # and those relying on copy on write to perform better.
   # Rake tasks automatically ignore this option for performance.
